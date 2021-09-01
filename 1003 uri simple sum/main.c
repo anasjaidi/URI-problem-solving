41bed9d927c7e2c8a1a6523727324ec84640649a
ANas jaidi <anas.jaidi@icloud.com>
2021-09-01 19:33:30 +0100

    1003 uri

---
+++ 1003 uri simple sum/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    int y;
+
+    // printf("write the first number : ");
+    scanf("%d", &x);
+    // printf("write the second number : ");
+    scanf("%d", &y);
+    printf("SOMA = %d\n", x + y);
+    return (0);
+}
